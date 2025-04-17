00cde004100fe9445c2377886caca10b2aea0989
Ailsa Don <ailsazawadi415@gmail.com>
2025-04-17 20:32:01 +0300

    Updated insert data and functions for product and sales

--- database.py
+++ database.py
@@ -1,10 +1,14 @@
 import psycopg2
+from datetime import datetime
 
 # Create a connection to db
 conn = psycopg2.connect(user="postgres", password="Zawadi@2006#",host="localhost",port="5432",database="myduka")
 
 # Executes database operations
 cur = conn.cursor()
+
+current_datetime = datetime.now()
+print("This is the time and date now: ",current_datetime)
 
 # Query
 def fetch_products():
@@ -26,10 +30,25 @@ def fetch_sales():
     for sale in sales:
         print(sale)
 
-# Calling the functions back
+# A query inserting a product
+def insert_product():
+    cur.execute("INSERT INTO products(name,buying_price,selling_price,stock_quantity) VALUES('Milk',50,60,100);")
+    # Saving operations
+    conn.commit()
+    return "Products Inserted"
+# # A query inserting a sale
+def insert_sale():
+    cur.execute(f"INSERT INTO sales(pid,quantity,created_at) VALUES(3,15,'{current_datetime}');")
+    # Saving operations
+    conn.commit()
+    return "Sales made"
+
+product = insert_product()
+sale = insert_sale()
+print(sale)
+print(product)
+
+
 fetch_products()
 fetch_sales()
-
-
-
 
