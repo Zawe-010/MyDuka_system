d9350e99f00996806c52717f1306b4a2e44afd7d
Ailsa Don <ailsazawadi415@gmail.com>
2025-04-22 16:11:28 +0300

    Additional code for both the products and sales

--- database.py
+++ database.py
@@ -52,3 +52,60 @@ print(product)
 fetch_products()
 fetch_sales()
 
+# Task(for fetch)
+
+def fetch_data(table):
+    cur.execute(f"SELECT * FROM {table};")
+    data = cur.fetchall()
+    return data
+
+# products = fetch_data('products')
+# sales = fetch_data('sales')
+# print("Products from fetch data func:\n",products)
+# print("Sales from fetch data func:\n",sales)
+
+# Task(for insert)
+# Method 1(takes values as parameters)
+def insert_products(values):
+    # Use of placeholders
+    insert = "INSERT INTO products(name, buying_price, selling_price, stock_quantity) VALUES(%s,%s,%s,%s)"
+    cur.execute(insert, values)
+    conn.commit()
+
+product_values = ("Meat", 120, 150, 200)
+insert_products(product_values)
+products = fetch_data('products')
+print("Fetching data after modified func.\n",products)
+
+def insert_sales(values):
+    insert_2 = "INSERT INTO sales(pid, quantity, created_at) VALUES(%s, %s, %s)"
+    cur.execute(insert_2, values)
+    conn.commit()
+
+# Fix datetime string formatting
+sales_values = (4, 20, current_datetime)
+insert_sales(sales_values)
+sales = fetch_data('sales')
+print("Fetching data after modified func.\n",sales)
+
+# Method 2 (take values as parameter but doesn't use placeholders).
+# Instead we replace placeholders with {values} parameter in a formatted string.
+# def insert_products_method(values):
+#     insert = f"INSERT INTO products(name, buying_price, selling_price, stock_quantity) VALUES{values}"
+#     cur.execute(insert)
+#     conn.commit()
+
+# product_values = (" Mango",40,50,100)
+# insert_products_method(product_values)
+# products = fetch_data('products')
+# print("Fetching prods method:\n",products)
+
+# def insert_products_method(values):
+#     insert = f"INSERT INTO sales(pid, quantity, created_at) VALUES{values}"
+#     cur.execute(insert)
+#     conn.commit()
+
+# sales_values_values = (10,25, current_datetime)
+# insert_products_method(sales_values)
+# sales = fetch_data('sales')
+# print("Fetching prods method:\n",sales)
