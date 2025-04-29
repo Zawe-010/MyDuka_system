53bb2c68a1c7c176af7738c389d99e3a2a0d34ca
Ailsa Don <ailsazawadi415@gmail.com>
2025-04-29 17:40:15 +0300

    New codes added to each html and py files

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Importing flask to use it
-from flask import Flask , render_template
-from database import fetch_products,fetch_sales
+from flask import Flask , render_template, request, redirect, url_for
+from database import fetch_products,fetch_sales,insert_products,insert_sales
 
 
 # Instantiate your application - initialization of Flask
@@ -21,10 +21,36 @@ def products():
     products = fetch_products()
     return render_template("products.html", products = products)
 
+@app.route('/add_products', methods=['POST'])
+def add_products():
+    if request.method == 'POST' : 
+        name = request.form['p-name']
+        buying_price = request.form['b-price']
+        selling_price = request.form['s-price']
+        stock_quantity =request.form['quantity']
+        new_product = (name, buying_price, selling_price, stock_quantity)
+        insert_products(new_product)
+        return redirect(url_for('products'))
+    
+
+@app.route('/add_sales', methods=['POST'])
+def add_sales():
+    if request.method == 'POST' : 
+        pid= request.form['p-id']
+        quantity = request.form['quantity']
+        created_at = request.form['created_at']
+        new_sale = (pid, quantity, created_at )
+        insert_sales(new_sale)
+        return redirect(url_for('sales'))
+
 @app.route('/sales')
 def sales():
     sales = fetch_sales()
     return render_template("sales.html", sales = sales)
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
 
 # Running your application
 app.run(debug=True)
